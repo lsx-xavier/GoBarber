b51b830379d57057f524d6c6cd485bfff479b337
impactoweb <57235888+impactoweb@users.noreply.github.com>
2020-02-11 17:58:41 -0300

    Middleware de autenticação

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,20 +1,27 @@
-import User from '../models/User'
+import User from '../models/User';
 
 class UserController {
   async store(req, res) {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
-if (userExists) {
-  return res.status(401).json({ error: `User already exists!` });
-}
+    if (userExists) {
+      return res.status(401).json({ error: `User already exists!` });
+    }
+
     const { id, name, email, provider } = await User.create(req.body);
 
     return res.json({
       id,
       name,
       email,
-      provider
+      provider,
     });
+  }
+
+  async update(req, res) {
+    console.log(req.userId);
+
+    return res.json({ ok: true });
   }
 }
 
