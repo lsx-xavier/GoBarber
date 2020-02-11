5a1b2c705aab20af6c48403cf80fa00f213511fb
impactoweb <57235888+impactoweb@users.noreply.github.com>
2020-02-11 15:54:13 -0300

    Start project

---
+++ src/database/index.js
@@ -0,0 +1,21 @@
+import sequelize from 'sequelize';
+
+import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
+
+const models = [User];
+
+class Database {
+  constructor() {
+    this.init()
+  }
+
+  init() {
+    this.connection = new   sequelize(databaseConfig);
+
+    models.map(model => model.init(this.connection));
+  }
+}
+
+export default new Database();
