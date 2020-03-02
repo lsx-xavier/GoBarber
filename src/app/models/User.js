9333ca49c31fd4c1934296cf897ba47cffc54fb2
impactoweb <57235888+impactoweb@users.noreply.github.com>
2020-03-02 15:45:25 -0300

    Feature avatar: Add the new feature, add the img to avatar profile

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -26,6 +26,10 @@ class User extends Model {
     return this;
   }
 
+  static associate(models) {
+    this.belongsTo(models.File, { forcignKey: 'avatar_id' });
+  }
+
   checkPassword(password) {
     return bcrypt.compare(password, this.password_hash);
   }
