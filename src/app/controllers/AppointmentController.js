2bf52e35dd64a029dbe02a69e30007633c8e4f47
Lucas Xavier <56982135+lsx-xavier@users.noreply.github.com>
2020-03-04 08:03:10 -0300

    Start Feature Notify: Notify the providers when have a new appointment.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,11 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -92,6 +94,22 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    /**
+     * Notify a provider
+     */
+
+    const user = await User.findByPk(req.userId);
+    const formmattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt }
+    );
+
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para ${formmattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
